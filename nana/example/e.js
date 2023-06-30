83485426895cf5843cbfe5abf100cdade68371ff
nnarangerel <136777197+nnarangerel@users.noreply.github.com>
2023-06-30 11:43:14 +0800

    Add files via upload

--- nana/example/e.js
+++ nana/example/e.js
@@ -100,38 +100,93 @@
 
 // }
 
-let too = [2, 234, 5, 43, 1];
+// let too = [2, 234, 5, 43, 1];
 
-const max = (too) => {
-  let maxNumber = too[0];
-  for (let i = 1; i < too.length; i++) {
-    const element = too[i];
-    if (element > maxNumber) maxNumber = element;
-  }
-  return maxNumber;
-};
+// const sort = (input) => {
+//   let array = input;
+// Loop
+// {
+// Find max
 
-const sort = (input) => {
-  let array = input;
-  // Loop
-  // {
-  // Find max
+// push max
 
-  // push max
+// remove max from array
 
-  // remove max from array
+// }
+//   let result = [];
+//   for (let i = 0; i < input.length; i++) {
+//     const element = input[i];
+//     const maxNumber = max(array);
+//     result.push(maxNumber);
 
-  // }
-  let result = [];
-  for (let i = 0; i < input.length; i++) {
-    const element = input[i];
-    const maxNumber = max(array);
-    result.push(maxNumber);
+//     // remove max from array
+//     const newArray = array.filter((el) => el !== maxNumber);
+//     array = newArray;
+//   }
+//   return result;
+// };
+// console.log(sort(too));
+// let hurd = prompt("hurd");
+// hurd = parseFloat(hurd);
+// let onoo = 0;
+// if (hurd > 80) {
+//   onoo = onoo + 1;
+//   console.log("-1");
+//   alert("-1");
+//   if (90 > hurd > 84) {
+//     onoo = 2;
+//     console.log("-2");
+//     alert("-2");
+//   }
+//   if (95 > hurd > 89) {
+//     onoo = 3;
+//     console.log("-3");
+//     alert("-3");
+//   }
+//   if (100 > hurd > 94) {
+//     onoo + 4;
+//     console.log("-4");
+//     alert("-4");
+//   }
+//   if (hurd > 99) {
+//     onoo + 5;
+//     console.log("erh hasuulna");
+//     alert("erh hasuulna");
+//   }
+// }
+// console.log(
+//   [1, 2, 3, 4].reduce((a, b) => a + b, 0)
+// )
+// console.log(
+//   [].reduce((a, b) => a + b, 0)
+// )
+// let sum = 0;
+// let o = 10;
+// for (let i = 0; i <= o; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     sum = sum + i;
+//     console.log("sum=" + sum);
+//   }
+// }
 
-    // remove max from array
-    const newArray = array.filter((el) => el !== maxNumber);
-    array = newArray;
-  }
-  return result;
-};
-console.log(sort(too));
+// let total = 0;
+// let p = 10;
+// for (let i = 0; i <= p; i++) {
+//   if (i % i === 0) {
+//     console.log(i);
+//   } else {
+//     console.log("non");
+//   }
+// }
+
+// let count = 0;
+// for (let i = 2; i <= 10; i++) {
+//   for (p = 1; p <= i; p++) {
+//     if (i % p == 0) count++;
+//   }
+
+//   if (count == 2) console.log(i);
+//   count = 0;
+// }
+
+let t = [2, 4, 4, 64, 65, 445];
